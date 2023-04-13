3f9e2104b17c664b94f34653beea5eaab0d936c4
Admin <Admin@123.com>
2023-04-13 17:14:49 +0800

    改变时间颜色示例

--- app/src/main/java/com/example/xapplication/Tutorial.java
+++ app/src/main/java/com/example/xapplication/Tutorial.java
@@ -1,13 +1,42 @@
 package com.example.xapplication;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import android.graphics.Color;
+import android.widget.TextView;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-
+//https://github.com/rovo89/XposedBridge/wiki/Development-tutorial#definingmodules
 
 public class Tutorial implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("Loaded app: " + lpparam.packageName);
+
+        if(lpparam.packageName.equals("com.android.systemui")){
+            findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook(){
+                //            @Override
+//            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                // this will be called before the clock was updated by the original method
+//            }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    // this will be called after the clock was updated by the original method
+                    TextView tv = (TextView) param.thisObject;
+                    String text = tv.getText().toString();
+                    tv.setText(text + " :)");
+                    tv.setTextColor(Color.RED);
+                }
+            });
+
+        }
+
+
     }
+
+
 }
